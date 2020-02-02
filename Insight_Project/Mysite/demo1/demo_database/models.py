d6b97b38ed39c6ec5676b40ed2244217da2d037f
User <user@mac-5272724.T-mobile.com>
2020-02-02 01:59:17 -0800

    update website

--- Insight_Project/Mysite/demo1/demo_database/models.py
+++ Insight_Project/Mysite/demo1/demo_database/models.py
@@ -1,18 +1,67 @@
 from django.db import models
+from django.contrib import admin
+import datetime
 
-# Create your models here.
 
 class Customer(models.Model):
-
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length=200)
 
     phonenumber = models.CharField(max_length=200)
 
     address = models.CharField(max_length=200)
 
-    qq = models.CharField(max_length=200, null=True, blank=True)
+    # qq = models.CharField(max_length=200, null=True, blank=True)
 
 
+class Medicine(models.Model):
+    # 药品名
+    name = models.CharField(max_length=200)
+    # 药品编号
+    sn = models.CharField(max_length=200)
+    # 描述
+    desc = models.CharField(max_length=200)
 
-from django.contrib import admin
+
+class Order(models.Model):
+    # 订单名
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # 创建日期
+    create_date = models.DateTimeField(default=datetime.datetime.now)
+    # 客户
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    # 订单购买的药品，和Medicine表是多对多 的关系
+    medicines = models.ManyToManyField(Medicine, through='OrderMedicine')
+
+
+class OrderMedicine(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    medicine = models.ForeignKey(Medicine, on_delete=models.PROTECT)
+
+    # 订单中药品的数量
+    amount = models.PositiveIntegerField()
+
+
 admin.site.register(Customer)
+
+'''
+CASCADE
+
+删除主键记录和 相应的外键表记录。
+
+比如，我们要删除客户张三，在删除了客户表中张三记录同时，也删除Order表中所有这个张三的订单记录
+
+PROTECT
+
+禁止删除记录。
+
+比如，我们要删除客户张三，如果Order表中有张三的订单记录，Django系统 就会抛出ProtectedError类型的异常，当然也就禁止删除 客户记录和相关的订单记录了。
+
+除非我们将Order表中所有张三的订单记录都先删除掉，才能删除该客户表中的张三记录。
+
+SET_NULL
+
+删除主键记录，并且将外键记录中外键字段的值置为null。 当然前提是外键字段要设置为值允许是null。
+
+比如，我们要删除客户张三时，在删除了客户张三记录同时，会将Order表里面所有的 张三记录里面的customer字段值置为 null。 但是上面我们并没有设置 customer 字段有 null=True 的参数设置，所以，是不能取值为 SET_NULL的。
+
+'''
--- Insight_Project/Mysite/demo1/mgr/customer.py
+++ Insight_Project/Mysite/demo1/mgr/customer.py
@@ -4,6 +4,22 @@ import json
 
 
 def dispatcher(request):
+
+    # 根据session判断用户是否是登录的管理员用户
+    if 'usertype' not in request.session:
+        return JsonResponse({
+            'ret': 302,
+            'msg': '未登录',
+            'redirect': '/mgr/sign.html'},
+            status=302)
+
+    if request.session[ 'usertype' ] != 'mgr':
+        return JsonResponse({
+            'ret': 302,
+            'msg': '用户非mgr类型',
+            'redirect': '/mgr/sign.html'},
+            status=302)
+
     # 将请求参数统一放入request 的 params 属性中，方便后续处理
 
     # GET请求 参数在url中，同过request 对象的 GET属性获取
